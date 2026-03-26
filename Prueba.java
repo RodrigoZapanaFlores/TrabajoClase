9ae1d42315914254250ddcdb546904576be51b3a
RodrigoZapanaFlores <Rodrigozapanaflores@gmail.com>
2026-03-26 16:23:46 +0100

    nuevos cambios

--- Prueba.java
+++ Prueba.java
@@ -1,12 +1,9 @@
 public class Prueba{
-<<<<<<< HEAD
  public static void main(String[] args){
     System.out.println("hola");
-
+    System.out.println("he aceptado los cambios");
 
     
  }
-=======
     // isdfjgodsnods
->>>>>>> dfc1a236f9000e72c1c834e77a1127ff60589ee2
 }
